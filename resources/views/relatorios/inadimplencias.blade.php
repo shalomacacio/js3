74d88e7c7a091713dbfaa6079c907166ed982c07
Shalom Acacio <shalom.acacio@gmail.com>
2021-04-06 11:00:44 -0300

    filter column

--- resources/views/relatorios/inadimplencias.blade.php
+++ resources/views/relatorios/inadimplencias.blade.php
@@ -70,54 +70,71 @@
 @endsection
 
 @section('javascript')
+
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/fixedheader/3.1.8/js/dataTables.fixedHeader.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.bootstrap4.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.colVis.min.js"></script>
 
 <script>
-$(document).ready(function() {
-    // Setup - add a text input to each footer cell
-    $('#example thead tr').clone(true).appendTo( '#example thead' );
-    $('#example thead tr:eq(1) th').each( function (i) {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="'+title+'" />' );
- 
-        $( 'input', this ).on( 'keyup change', function () {
-            if ( table.column(i).search() !== this.value ) {
-                table
-                    .column(i)
-                    .search( this.value )
-                    .draw();
-            }
-        } );
-    } );
- 
-    var table = $('#example').DataTable( {
-        processing: true,
-        lengthChange: false,
-        buttons: [ 
-                    'excel', 
-                    'csvHtml5',
-                    'pdfHtml5',
-                ],
-        paging:   false,
-        info:     true,
-        bFilter: true,
-        ordering: true,
-        pageLength: 100,
-        language: {
-            search: "Pesquisar",
-            info: "Mostrando de _START_ até _END_ de _TOTAL_ registros",
-            paginate: {
-            "next": "Próximo",
-            "previous": "Anterior",
-            "first": "Primeiro",
-            "last": "Último"
-            },
-        }
-    });
+        
+    $(document).ready(function() {
 
-} );
+        // Setup - add a text input to each footer cell
+        $('#example thead tr').clone(true).appendTo( '#example thead' );
+        $('#example thead tr:eq(1) th').each( function (i) {
+            var title = $(this).text();
+            $(this).html( '<input type="text" placeholder="'+title+'" />' );
+    
+            $( 'input', this ).on( 'keyup change', function () {
+                if ( table.column(i).search() !== this.value ) {
+                    table
+                        .column(i)
+                        .search( this.value )
+                        .draw();
+                }
+            } );
+        });
+    
+        var table = $('#example').DataTable({
+            processing: true,
+            lengthChange: false,
+            buttons: [ 
+                        'excel', 
+                        'csvHtml5',
+                        {
+                            extend: 'pdfHtml5',
+                            orientation: 'landscape',
+                            pageSize: 'LEGAL'
+                        }
+                    ],
+            paging:   false, //paginação
+            info:     true, //mostrando 1 de x paginas 
+            bFilter: true, //campo pesquisa 
+            ordering: true, // ordenação
+            pageLength: 100, //por pagina 
+            language: {
+                search: "Pesquisar",
+                info: "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                paginate: {
+                "next": "Próximo",
+                "previous": "Anterior",
+                "first": "Primeiro",
+                "last": "Último"
+                },
+            }
+        });
+
+        table.buttons().container()
+        .appendTo( '#example_wrapper .col-md-6:eq(0)' );
+
+    } );
 </script>
 @endsection
